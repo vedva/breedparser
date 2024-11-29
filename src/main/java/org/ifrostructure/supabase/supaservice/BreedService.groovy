a2ed8a05a2725835325f3c5bf504de3b8288b523
vedva <vedva02@gmail.com>
2024-11-29 00:17:08 -0500

    add BreedDB, SupabaseHTTPI; edit BreedService, SupabaseHTTP, UTBreedService

--- src/main/java/org/ifrostructure/supabase/supaservice/BreedService.groovy
+++ src/main/java/org/ifrostructure/supabase/supaservice/BreedService.groovy
@@ -1,32 +1,44 @@
 package org.ifrostructure.supabase.supaservice
 
-import org.breed.Breed
-import org.ifrostructure.supabase.supabaseconnector.SupabaseHTTP
-import org.utiles.EnvReader
+import groovy.json.JsonSlurper
+import org.breed.BreedDB
+import org.ifrostructure.supabase.supabaseconnector.SupabaseHTTPI
+import java.net.http.HttpResponse
 
 class BreedService {
     private final static String END_POINT = "/rest/v1/dogs"
-    private SupabaseHTTP sc;
+    private SupabaseHTTPI sc;
 
-    BreedService(SupabaseHTTP sc) {
+    BreedService(SupabaseHTTPI sc) {
         this.sc = sc
     }
 
-    List<Breed> getAllBreedsFromSupa(){
-        def url = EnvReader.getEnvVar("SUPABASE_URL") + END_POINT
-        def apiKey = EnvReader.getEnvVar("SUPABASE_PUBLIC_KEY")
+    List<BreedDB> getAllBreedsFromSupa() {
+        HttpResponse<String> response = sc.getRequest(END_POINT, [:], [:])
+        if (response == null) {
+            println "ERROR: HTTPResponse is null"
+            return []
+        }
+        if (response.statusCode() != 200) {
+            println "ERROR: Status code is not 200: ${response.statusCode()}"
+            return []
+        }
+        def jsonSlurper = new JsonSlurper()
+        List<BreedDB> result = []
+        try {
+            // Parse the JSON response into a List of Maps
+            List<Map> jsonResponse = jsonSlurper.parseText(response.body().toString())
 
-        // Make HTTP GET request
-        def response = Request.get(url)
-                .addHeader("apikey", apiKey)
-                .addHeader("Authorization", "Bearer $apiKey")
-                .execute()
-                .returnContent()
-                .asString()
+            // Convert each Map into a Breed object
+            result = jsonResponse.collect { map ->
+                new BreedDB((Integer) map.id, (String)map.breed, (String)map.image, (String)map.link, (String)map.article)
+            }
+        } catch (Exception e) {
+            println "ERROR: jsonSlurper exception: $e.message"
+            return []
+        }
 
-        return breeds
+        return result
     }
-
-
 
 }
--- src/test/java/org/ifrostructure/supabase/supabaservice/UTBreedService.groovy
+++ src/test/java/org/ifrostructure/supabase/supabaservice/UTBreedService.groovy
@@ -1,4 +1,28 @@
 package org.ifrostructure.supabase.supabaservice
 
+import org.breed.BreedDB
+import org.ifrostructure.supabase.supabaseconnector.SupabaseHTTP
+import org.ifrostructure.supabase.supaservice.BreedService
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+import org.utiles.EnvReader
+
 class UTBreedService {
+    private static final String TEST_SUPABASE_URL = EnvReader.getEnvVar("SUPABASE_URL")
+    private static final String TEST_API_KEY = EnvReader.getEnvVar("SUPABASE_PUBLIC_KEY")
+
+    private SupabaseHTTP supabaseHTTP
+
+    @BeforeMethod
+    void setup() {
+        supabaseHTTP = new SupabaseHTTP(TEST_SUPABASE_URL, TEST_API_KEY)
+    }
+
+    @Test
+    void testGetBreedDB() {
+        BreedService breedService = new BreedService(supabaseHTTP)
+        List<BreedDB> breedsDB = breedService.getAllBreedsFromSupa()
+        println(breedsDB)
+
+    }
 }
